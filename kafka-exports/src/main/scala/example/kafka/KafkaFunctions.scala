cbb3de4dc32204c3c3137234033535f901af3bbe
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-11 15:25:25 +0000

    -

--- kafka-exports/src/main/scala/example/kafka/KafkaFunctions.scala
+++ kafka-exports/src/main/scala/example/kafka/KafkaFunctions.scala
@@ -11,7 +11,7 @@ import example.model.{Person, PersonKey}
   */
 trait KafkaFunctions:
   /** kafka code generation uses 2 sets of params with the 1st one containing the kafka key and the 2nd set any data we want to publish to kafka. The kafka key
-    * can be a primitive (string, int etc) but for this example we use a case class.
+    * can be a primitive (string, int etc) but for this example we use case class PersonKey.
     */
   def addPerson(key: PersonKey)(ttl: Long, person: Person): Unit
   def removePerson(key: PersonKey)(person: Person): Unit
